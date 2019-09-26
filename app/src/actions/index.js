b9eb6eddee7ac539d136ab0eb2b54feac74ebffc
Jason Loomis <jason@jrloomis.com>
2019-09-26 01:45:28 -0400

    changed function name

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -4,9 +4,13 @@ export const FETCHING = "FETCHING";
 export const FETCH_SUCCESS = "FETCH_SUCCESS";
 export const FETCH_FAILURE = "FETCH_FAILURE";
 
-export const getFilm = () => dispatch => {
+// Again with being generic
+// The function might be too generic
+// Would be if doing more than one API call
+
+export const getData = () => dispatch => {
   // A different way to return a function - shown in the TK videos
-  // Might be confusing because it isn't as explicit as Brian's lecture
+  // Might be confusing because it might not look as explicit as Brian's lecture
   dispatch({ type: FETCHING });
   axios
     .get("https://ghibliapi.herokuapp.com/films")
@@ -15,4 +19,5 @@ export const getFilm = () => dispatch => {
   // Another trick learned from the TK videos
   // console.log(error.response) returns an object with this information
   // just intentionally mess up the .get url to see it/test it
+  // In this case, payload is "404 Not Found", and that prints to screen
 };
