bf1a8dc570b17dd60bb708b75d5f52a7ce45e41f
Jason Loomis <jason@jrloomis.com>
2019-09-26 01:02:11 -0400

    import PageHeader and Movies

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+
+import PageHeader from "./components/PageHeader";
+import Movies from "./components/Movies";
+
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <div>Div!</div>
+      <PageHeader />
+      <Movies />
     </div>
   );
 }
