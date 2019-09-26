2b169169c4323da42cfe062216ff42b221e58abc
Jason Loomis <jason@jrloomis.com>
2019-09-26 01:44:10 -0400

    axios call and dispatches working

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -1,17 +1,18 @@
 import axios from "axios";
 
-export const FETCHING = "FETCH_FILM_START";
-export const FETCH_SUCCESS = "FETCH_FILM_SUCCESS";
-export const FETCH_FAILURE = "FETCH_FILM_FAILURE";
+export const FETCHING = "FETCHING";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_FAILURE = "FETCH_FAILURE";
 
 export const getFilm = () => dispatch => {
+  // A different way to return a function - shown in the TK videos
+  // Might be confusing because it isn't as explicit as Brian's lecture
   dispatch({ type: FETCHING });
   axios
-    .get("")
-    .then(resolve => {
-      console.log(resolve.data);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+    .get("https://ghibliapi.herokuapp.com/films")
+    .then(resolve => dispatch({ type: FETCH_SUCCESS, payload: resolve.data }))
+    .catch(error => dispatch({ type: FETCH_FAILURE, payload: `${error.response.status} ${error.response.statusText}` }));
+  // Another trick learned from the TK videos
+  // console.log(error.response) returns an object with this information
+  // just intentionally mess up the .get url to see it/test it
 };
