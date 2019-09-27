1c96f6641ba95c024407f8348e918125c20dbe97
Jason Loomis <jason@jrloomis.com>
2019-09-26 23:16:43 -0400

    slight change to getData

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -6,11 +6,9 @@ export const FETCH_FAILURE = "FETCH_FAILURE";
 
 const baseURL = "https://ghibliapi.herokuapp.com/";
 
-export const getData = () => {
-  return function(dispatch) {
-    axios
-      .get(baseURL + "films")
-      .then(resolve => dispatch({ type: FETCH_SUCCESS, payload: resolve.data }))
-      .catch(error => dispatch({ type: FETCH_FAILURE, payload: `${error.response.status} ${error.response.statusText}` }));
-  };
+export const getData = () => dispatch => {
+  axios
+    .get(baseURL + "films")
+    .then(resolve => dispatch({ type: FETCH_SUCCESS, payload: resolve.data }))
+    .catch(error => dispatch({ type: FETCH_FAILURE, payload: `${error.response.status} ${error.response.statusText}` }));
 };
