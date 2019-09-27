81e8a28e342dac6cddfd2dd2e3e412cd04fde622
Jason Loomis <jason@jrloomis.com>
2019-09-26 23:05:42 -0400

    connected to store to pass state to movies.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,52 @@
 import React from "react";
+import { connect } from "react-redux";
+import { getData } from "./actions";
 
-import PageHeader from "./components/PageHeader";
-import Movies from "./components/Movies";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Paper } from "@material-ui/core";
 
 import "./App.css";
 
-function App() {
+import Nav from "./components/Nav";
+import Movies from "./components/Movies";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    paddingTop: theme.spacing(5),
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  }
+}));
+
+const App = props => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <PageHeader />
-      <Movies />
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Grid component="header" container>
+          <Grid component="h1" item xs={12}>
+            Studio Ghibli
+          </Grid>
+          <Grid component="nav" item xs={12}>
+            <Nav />
+          </Grid>
+        </Grid>
+      </Paper>
+      <Movies movie={props.movie} />
     </div>
   );
-}
+};
 
-export default App;
+const mapStateToProps = state => ({
+  movie: state.data,
+  error: state.error
+});
+//
+export default connect(
+  mapStateToProps,
+  { getData }
+)(App);
